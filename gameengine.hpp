6a1555563ba3abe96dee326e89d38ec0318f6c8e
bagons <xyz@xyz.xyz>
2025-07-23 11:50:16 +0200

    update gameengine.hpp due to all the changes

--- gameengine.hpp
+++ gameengine.hpp
@@ -1,7 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-class Engine;
-Engine* gameengine();
+class Thing;
+class Camera;
+
+class Engine {
+public:
+    unsigned int base_vertex_shader;
+    std::vector<Camera *> cameras;
+    std::vector<Thing *> things;
+    void render(int camera_index);
+    void send_it_to_window();
+    bool is_running();
+};
+Engine* gameengine(const char* game_name);
+extern Engine ge;
+
+int noengine();
 
 #endif //MAIN_H
