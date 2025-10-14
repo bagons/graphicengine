a782cbb933290665320018b64edfc239df6e5162
bagons <xyz@xyz.xyz>
2025-10-14 15:35:01 +0200

    remove old shader compilation methods

--- include/utils.h
+++ include/utils.h
@@ -3,7 +3,6 @@
 #pragma once
 #include <glad/glad.h>
 
-unsigned int compile_shader_from_file(const char* file_path, GLenum shader_type);
 unsigned int setup_texture_from_file(const char* file_path, bool generate_minimaps=true);
 unsigned int compile_shader_from_string(const char* shader_code, GLenum shader_type);
 
--- utils.cpp
+++ utils.cpp
@@ -6,52 +6,8 @@
 #include "utils/stb_image.h"
 #include "meshes.hpp"
 
-unsigned int compile_shader_from_file(const char* file_path, GLenum shader_type) {
-    unsigned int shader = glCreateShader(shader_type);
-    std::cout << "loading shader file: " << file_path << std::endl;
-    std::string shader_string;
 
-    std::ifstream file(file_path);
-    std::cout << "file_read_state: " << file.good() << std::endl;
-    std::string line;
-    while (std::getline(file, line)) {
-        shader_string += line + '\n';
-    }
-    file.close();
 
-    file_path = shader_string.c_str();
-    glShaderSource(shader, 1, &file_path, nullptr);
-    glCompileShader(shader);
-
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    return shader;
-}
-
-unsigned int compile_shader_from_string(const char* shader_code, GLenum shader_type) {
-    unsigned int shader = glCreateShader(shader_type);
-    glShaderSource(shader, 1, &shader_code, nullptr);
-    glCompileShader(shader);
-
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    return shader;
-}
 
 unsigned int setup_texture_from_file(const char* file_path, bool generate_minimaps) {
     unsigned int texture;
